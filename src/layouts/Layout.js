c0e76dd1f6a2805cf12d9adc9181351201873bf4
Grzegorz Bors <35344255+grzegorzbors@users.noreply.github.com>
2021-04-14 21:46:13 +0200

    corrected structure

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,6 +1,7 @@
-import Header from "./Header";
-import Content from "./Content";
-import Footer from "./Footer";
+import Header from "../components/Header";
+import Content from "../components/Content";
+import Footer from "../components/Footer";
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 const Layout = () => {
--- src/layouts/Logo.js
+++
@@ -1,9 +0,0 @@
-import useMyStyles from "../styles/MyStyles";
-
-const Logo = () => {
-  const classes = useMyStyles();
-
-  return <div className={classes.logo}>Logo</div>;
-}
-
-export default Logo;
