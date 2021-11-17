4d9c9dad4698d8217db58a3ef522d59d8ec99127
grzegorzbors <gbors@o2.pl>
2021-11-17 18:36:48 +0100

    added policies config, Item and List components, implemented list in PrivacyPolicy page

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,4 +1,5 @@
 import { Grid } from "@material-ui/core";
+import PrivacyPoliciesList from "../components/PrivacyPolicy/PrivacyPoliciesList";
 import common from "../styles/common";
 
 const PrivacyPolicy = () => {
@@ -7,6 +8,7 @@ const PrivacyPolicy = () => {
   return (
     <Grid container className={classesCommon.pageFlexColumnLayout}>
       <h1>Polityka Prywatności</h1>
+      <PrivacyPoliciesList />
     </Grid>
   );
 };
---
+++ src/styles/privacyPolicy.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const privacyPolicy = makeStyles(() => ({
+  list: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  cardDesktop: {
+    width: "70%",
+  },
+}));
+
+export default privacyPolicy;
